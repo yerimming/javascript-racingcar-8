c08f8b71c48662ca0bbb48fa7b8c0a38a739cb38
Kimyerim <yeye092302@gmail.com>
2025-10-24 15:00:21 +0900

    feat(message): 메시지 상수 분리 및 에러 메시지 구분

---
+++ src/constants/message.js
@@ -0,0 +1,14 @@
+export const MESSAGES = {
+    INPUT_CAR_NAMES: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+    INPUT_TRY_COUNT: "\n시도할 횟수는 몇 회인가요?\n",
+    START: "\n실행 결과",
+    WINNER: "최종 우승자 : ",
+    NO_WINNER: "우승자가 없습니다.",
+};
+
+export const ERROR_MESSAGES = {
+    PREFIX: "[ERROR]",
+    NAME_LENGTH: "자동차 이름은 5자 이하만 가능합니다.",
+    TRY_COUNT: "시도 횟수는 1 이상의 숫자여야 합니다.",
+    INPUT: "입력 오류가 발생했습니다.",
+}
