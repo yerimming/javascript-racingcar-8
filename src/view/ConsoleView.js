909ff4f99c160668d725fc0b640fe4ae076c200e
Kimyerim <yeye092302@gmail.com>
2025-10-24 16:34:07 +0900

    feat(ConsoleView): 시도 횟수 입력 및 에러 출력 기능 추가

--- src/view/ConsoleView.js
+++ src/view/ConsoleView.js
@@ -1,9 +1,20 @@
 import { Console } from "@woowacourse/mission-utils";
-import { MESSAGES, ERROR_MESSAGES } from "../constants/message";
+import { MESSAGES } from "../constants/message";
 
 class ConsoleView {
     async getCarNames() {
         const input = await Console.readLineAsync(MESSAGES.INPUT_CAR_NAMES);
         return input.split(",").map((name) => name.trim());
     }
+
+    async getTryCount() {
+        const input = await Console.readLineAsync(MESSAGES.INPUT_TRY_COUNT);
+        return Number(input);
+    }
+
+    printError(error) {
+        Console.print(`${error.message}`);
+    }
 }
+
+export default RacingController;
