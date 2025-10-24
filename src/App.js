524e2ca9ee25ef61f14ea79cb112b0ccbc245f97
Kimyerim <yeye092302@gmail.com>
2025-10-24 16:55:57 +0900

    feat(App): RacingController 실행 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import RacingController from "./controller/RacingController";
+
 class App {
-  async run() {}
+  async run() {
+    const controller = new RacingController();
+    await controller.run();
+  }
 }
 
 export default App;
